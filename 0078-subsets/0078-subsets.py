46fc2d8f85452b107abcf970809b426149cfd73d
Naitik Pandey <71080891+naitik2004@users.noreply.github.com>
2025-05-17 00:58:41 +0530

    Time: 0 ms (100%), Space: 12.7 MB (20.43%) - LeetHub

---
+++ 0078-subsets/0078-subsets.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    ans = []
+    result = []
+    i = 0
+    def add_sub(self,nums,ans,result,i):
+        if i == len(nums):
+            result.append(ans)
+            return
+        self.add_sub(nums, ans + [nums[i]], result, i+1)
+        self.add_sub(nums, ans, result, i+1)
+
+    def subsets(self, nums):
+        self.result = []
+        self.add_sub(nums, [], self.result, 0)
+        return self.result
+
