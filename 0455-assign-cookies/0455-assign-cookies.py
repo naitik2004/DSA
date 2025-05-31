384b21ec862868990cada9095cd93efd674c02d1
Naitik Pandey <71080891+naitik2004@users.noreply.github.com>
2025-05-31 09:45:26 +0530

    Time: 37 ms (29.98%), Space: 14.1 MB (73.6%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def findContentChildren(self, g, s):
+        g.sort()
+        s.sort()
+        count = 0
+        i,j = 0,0
+        while i < len(g) and j < len(s):
+            if s[j] >= g[i]:
+                i += 1
+                j += 1
+            else:
+                j += 1
+        return i
+
+            
