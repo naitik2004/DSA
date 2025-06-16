07e66b75cc070332b7ed2697fac6d021220c89a0
Naitik Pandey <71080891+naitik2004@users.noreply.github.com>
2025-06-17 00:12:36 +0530

    Time: 0 ms (100%), Space: 12.1 MB (100%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -2,13 +2,14 @@ class Solution(object):
     def sortColors(self, nums):
         count0 = 0
         count1 = 0
-
+        count2 = 0
         for i in nums:
             if i == 0:
                 count0 += 1 
             elif i == 1:
                 count1 += 1
-
+            else:
+                count2 += 1
 
         for i in range(len(nums)):
             if i < count0:
