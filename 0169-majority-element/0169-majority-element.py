dd46a00b09dd7cba906c71253a7f323c3e190e95
Naitik Pandey <71080891+naitik2004@users.noreply.github.com>
2025-06-29 23:27:31 +0530

    Time: 9 ms (34.09%), Space: 19.4 MB (50.96%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,15 +1,29 @@
-class Solution(object):
-    def majorityElement(self, nums):
-        avg = len(nums)//2
- 
-        var = set(nums)
-        myList = list(var)
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        ans = 0 
+        count = 0
+        for i in range(len(nums)):
+            if count == 0:
+                ans = nums[i]
+
+            if ans == nums[i]:
+                count += 1
+            else:
+                count -= 1
+        return ans
 
 
-        for i in range(len(myList)):
-            final = nums.count(myList[i])
-            if final > avg:
-                return(myList[i])
+
+        # n = (len(nums)//2)
+        # a = {}
+        # for i in nums:
+        #     if i in a:
+        #         a[i] += 1
+        #     else:
+        #         a[i] = 1
+
+        # for i, v in enumerate(a):
+        #     if a[v] > n:
+        #         return(v)
 
 
-        
