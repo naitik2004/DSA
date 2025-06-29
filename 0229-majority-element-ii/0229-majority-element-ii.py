cec9c3214b5622e905f8b9ffa6327d04bf1a16ea
Naitik Pandey <71080891+naitik2004@users.noreply.github.com>
2025-06-29 23:02:24 +0530

    Time: 11 ms (30.37%), Space: 22.4 MB (33.74%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,5 +1,33 @@
 class Solution:
-    def majorityElement(self, nums: List[int]) -> List[int]:
+    def majorityElement(self, nums: List[int]) -> List[int]: 
+        # Moore's voting algo...
+        
+        # a = []
+        # rep = len(nums)//3
+        # test = nums[0]
+        # count = 1
+
+        # if len(nums) <= 2:
+        #     return nums
+
+        # for i in range(len(nums)):
+        #     if count != 0:
+        #         if nums[i] == test:
+        #             count += 1
+        #         else:
+        #             count -= 1
+        #     elif count > rep:
+        #         a.append(nums[i])
+        #     else:
+        #         test = nums[i]
+        #         count = 1
+        # return a
+
+
+            
+
+
+
         a = {}
         res = []
         n = len(nums)
