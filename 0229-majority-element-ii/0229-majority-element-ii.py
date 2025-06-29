8ab4fd5b190d664638b1a63e2f0280e619ad6d50
Naitik Pandey <71080891+naitik2004@users.noreply.github.com>
2025-06-30 00:14:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,43 +1,62 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]: 
         # Moore's voting algo...
+        if len(nums) < 2:
+            return nums
+
+        res = []
+        first = nums[0]
+        second = None
+        fc = 1
+        sc = 0
+        for i in range(len(nums)):
+            if first == nums[i]:
+                first = nums[i]
+                fc += 1
+            elif second == nums[i]:
+                second = nums[i]
+                sc += 1
+            elif fc == 0:
+                first = nums[i]
+                fc = 1
+            elif sc == 0:
+                second = nums[i]
+                sc = 1
+            else:
+                fc -= 1
+                sc -= 1
         
-        # a = []
-        # rep = len(nums)//3
-        # test = nums[0]
-        # count = 1
-
-        # if len(nums) <= 2:
-        #     return nums
-
-        # for i in range(len(nums)):
-        #     if count != 0:
-        #         if nums[i] == test:
-        #             count += 1
-        #         else:
-        #             count -= 1
-        #     elif count > rep:
-        #         a.append(nums[i])
-        #     else:
-        #         test = nums[i]
-        #         count = 1
-        # return a
+        fc = 0
+        sc = 0
+        for i in nums:
+            if first == i:
+                fc += 1
+            if second == i:
+                sc += 1
+        
+        a = len(nums)//3
+        if fc > a:
+            res.append(first)
+        if sc > a:
+            res.append(second)
+        
+        return res
 
 
             
 
 
 
-        a = {}
-        res = []
-        n = len(nums)
-        rep = n/3
-        for i in nums:
-            if i in a:
-                a[i] += 1
-            else:
-                a[i] = 1
-        for i,j in enumerate(a):
-            if a[j] > rep:
-                res.append(j)
-        return(res)
+        # a = {}
+        # res = []
+        # n = len(nums)
+        # rep = n/3
+        # for i in nums:
+        #     if i in a:
+        #         a[i] += 1
+        #     else:
+        #         a[i] = 1
+        # for i,j in enumerate(a):
+        #     if a[j] > rep:
+        #         res.append(j)
+        # return(res)
