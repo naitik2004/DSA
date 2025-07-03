c8ac40e9cf2cc4817f06d5c68f2a109e3b60b82a
Naitik Pandey <71080891+naitik2004@users.noreply.github.com>
2025-07-04 00:42:56 +0530

    Time: 3 ms (50.37%), Space: 18.7 MB (62.43%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -20,12 +20,28 @@
 #                 res.append(indexed_nums[j][1])
 #                 break
 #         return res
+
+
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         l=[]
+#         for i in range (len(nums)):
+#             for j in range (i+1,len(nums)):
+#                 if nums[i]+nums[j]==target:
+#                     l.append(i)
+#                     l.append(j)
+#         return l
+
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        l=[]
-        for i in range (len(nums)):
-            for j in range (i+1,len(nums)):
-                if nums[i]+nums[j]==target:
-                    l.append(i)
-                    l.append(j)
-        return l
+        numMap = {}
+        n = len(nums)
+
+        for i in range(n):
+            complement = target - nums[i]
+            if complement in numMap:
+                return [numMap[complement], i]
+            numMap[nums[i]] = i
+
+        return []  # No solution found
