bee84f4f93e85933cd369351ce589dbcae61e97e
Naitik Pandey <71080891+naitik2004@users.noreply.github.com>
2025-08-19 22:58:04 +0530

    Time: 94 ms (10.78%), Space: 20.8 MB (22.41%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.py
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.py
@@ -5,15 +5,9 @@ class Solution(object):
         for i in range(len(nums)):
             if nums[i] == 0:
                 count += 1
-                if i+1 <= len(nums)-1 and nums[i+1] != 0:
-                    while count != 0:
-                        ans += count
-                        count -= 1
-                elif i == len(nums)-1:
-                    while count != 0:
-                        ans += count
-                        count -= 1
+                if i+1 == len(nums) or nums[i+1] != 0:
+                    ans += count * (count + 1) // 2
+                    count = 0
             else:
                 count = 0
         return ans
-
