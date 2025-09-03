17b6bd3de179106d2193e4670301c66d31b40928
Naitik Pandey <71080891+naitik2004@users.noreply.github.com>
2025-09-03 21:08:39 +0530

    Time: 0 ms (100%), Space: 12.4 MB (62.3%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -6,21 +6,15 @@
 #         self.right = right
 class Solution(object):
     def rightSideView(self, root):
-        if root == None:
-            return []
-        
-        ans = []
-        max_lvl = -1
-        stack = [(root,0)]
-        while stack:
-            node, lvl = stack.pop()
+        result = []
+        def dfs(root,lvl):
+            if root == None:
+                return 
 
-            if lvl > max_lvl:
-                ans.append(node.val)
-                max_lvl = lvl
-            
-            if node.left:
-                stack.append((node.left, lvl + 1))
-            if node.right:
-                stack.append((node.right, lvl + 1))
-        return ans
+            if lvl == len(result):
+                result.append(root.val)
+
+            dfs(root.right, lvl + 1)
+            dfs(root.left, lvl + 1)
+        dfs(root,0)
+        return result
