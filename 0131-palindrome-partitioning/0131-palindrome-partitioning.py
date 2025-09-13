f9f4cc51c0645a74e276a471a7ae6f4db5a84ff7
Naitik Pandey <71080891+naitik2004@users.noreply.github.com>
2025-09-13 20:49:45 +0530

    Time: 75 ms (79.83%), Space: 48.1 MB (26.47%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,26 +1,43 @@
 class Solution(object):
-    def isPalidrome(self, s): 
-        s2 = s[::-1]
-        return (s == s2)
-
-
-        
-    
-    def getAllParts(self, s, partitions, ans):
-        if len(s) == 0:
-            ans.append(partitions[:])
-            return
-
-        for i in range(len(s)):
-            part = s[:i+1]
-            if self.isPalidrome(part):
-                partitions.append(part)
-                self.getAllParts(s[i+1:], partitions, ans)
-                partitions.pop()
-
-
     def partition(self, s):
-        self.partitions = []
-        self.ans = []
-        self.getAllParts(s, self.partitions, self.ans)
-        return self.ans
+        ans = []
+        def find(i, curr):
+            if i == len(s):
+                ans.append(curr[:])
+                return
+
+            for j in range(i, len(s)):
+                part = s[i:j+1]
+                if part == part[::-1]:
+                    curr.append(part)
+                    find(j+1,curr)
+                    curr.pop()
+        find(0,[])
+        return ans
+
+
+
+# class Solution(object):
+#     def isPalidrome(self, part): 
+#         s2 = part[::-1]
+#         return (part == s2)
+                
+    
+#     def getAllParts(self, s, partitions, ans):
+#         if len(s) == 0:
+#             ans.append(partitions[:])
+#             return
+
+        # for i in range(len(s)):
+        #     part = s[:i+1]
+        #     if self.isPalidrome(part):
+        #         partitions.append(part)
+        #         self.getAllParts(s[i+1:], partitions, ans)
+        #         partitions.pop()
+
+
+#     def partition(self, s):
+#         self.partitions = []
+#         self.ans = []
+#         self.getAllParts(s, self.partitions, self.ans)
+#         return self.ans
