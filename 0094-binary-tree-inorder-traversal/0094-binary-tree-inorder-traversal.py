adffb409660f695b69414247a316081413ad6922
Naitik Pandey <71080891+naitik2004@users.noreply.github.com>
2025-10-02 00:00:50 +0530

    Time: 0 ms (100%), Space: 12.5 MB (15.53%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
@@ -6,13 +6,33 @@
 #         self.right = right
 class Solution(object):
     def inorderTraversal(self, root):
-        a = []
-        def inOrder(root):
-            if root is None:
-                return 
-            inOrder(root.left)
-            a.append(root.val)
-            inOrder(root.right)
-        inOrder(root)
-        return a
+        # a = []
+        # def inOrder(root):
+        #     if root is None:
+        #         return 
+        #     inOrder(root.left)
+        #     a.append(root.val)
+        #     inOrder(root.right)
+        # inOrder(root)
+        # return a
         
+        if root == None:
+            return []
+
+        ans = []
+        stack = []
+        curr = root
+
+        while stack or curr:
+            # go left as possible
+            while curr:
+                stack.append(curr)
+                curr = curr.left
+            # now select one by one first the left then its rigth
+            # visit the Node
+            curr = stack.pop()
+            ans.append(curr.val)
+            # go to teh right child
+            curr = curr.right
+        return ans
+
